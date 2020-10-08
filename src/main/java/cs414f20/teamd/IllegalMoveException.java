6dd2419e28e7f6f54c6e01136449377db4a4ca19
Nick Cunningham <nic1571@rams.colostate.edu>
2020-10-08 09:07:01 -0600

    Add Exception Classes - Add IllegalMoveException.java file - Add IllegalPositionException.java file

---
+++ src/main/java/cs414f20/teamd/IllegalMoveException.java
@@ -0,0 +1,8 @@
+package cs414f20.teamd;
+
+@SuppressWarnings("serial")
+public class IllegalMoveException extends Exception {
+    public IllegalMoveException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/cs414f20/teamd/IllegalPositionException.java
@@ -0,0 +1,8 @@
+package cs414f20.teamd;
+
+@SuppressWarnings("serial")
+public class IllegalPositionException extends Exception {
+    public IllegalPositionException(String message) {
+        super(message);
+    }
+}
