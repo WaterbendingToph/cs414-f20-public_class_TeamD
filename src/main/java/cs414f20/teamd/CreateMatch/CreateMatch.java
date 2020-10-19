b5612a1d3bbb6e2f87eb59e9b0df060acae38769
randomguy6 <32748027+randomguy6@users.noreply.github.com>
2020-10-18 20:29:11 -0600

    Put up a new controller

---
+++ src/main/java/cs414f20/teamd/CreateMatch/CreateMatch.java
@@ -0,0 +1,13 @@
+package cs414f20.teamd.CreateMatch;
+
+public class CreateMatch {
+    private long gameID;
+
+    public CreateMatch(long gameID) {
+        this.gameID = gameID;
+    }
+
+    public long getGameID() {
+        return gameID;
+    }
+}
