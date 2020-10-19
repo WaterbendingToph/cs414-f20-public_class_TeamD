5f6fddcc7b8f65dffbc6d0b00a6a6d907857ad61
randomguy6 <32748027+randomguy6@users.noreply.github.com>
2020-10-18 20:29:11 -0600

    Put up a new controller

---
+++ src/main/java/cs414f20/teamd/CreateMatch/CreateMatchController.java
@@ -0,0 +1,18 @@
+package cs414f20.teamd.CreateMatch;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class CreateMatchController {
+
+    @GetMapping("/createMatch")
+    public CreateMatch createMatch() {
+        Random r = new Random();
+        long id = r.nextLong();
+        return new CreateMatch(Math.abs(id));
+    }
+}
