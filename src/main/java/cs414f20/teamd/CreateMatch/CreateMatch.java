575ff3e0ebf10d38f49a68eeb6e1062a4b2fc41e
randomguy6 <32748027+randomguy6@users.noreply.github.com>
2020-10-20 20:04:56 -0600

    Add some elements to controller

--- src/main/java/cs414f20/teamd/CreateMatch/CreateMatch.java
+++ src/main/java/cs414f20/teamd/CreateMatch/CreateMatch.java
@@ -1,13 +1,27 @@
 package cs414f20.teamd.CreateMatch;
 
+import java.time.LocalDate;
+
 public class CreateMatch {
     private long gameID;
+    private String opponent;
+    private java.time.LocalDate date;
 
-    public CreateMatch(long gameID) {
+    public CreateMatch(long gameID, String opponent) {
         this.gameID = gameID;
+        this.opponent = opponent;
+        this.date = java.time.LocalDate.now();
     }
 
     public long getGameID() {
         return gameID;
+    }
+
+    public String getOpponnet(){
+        return opponent;
+    }
+
+    public java.time.LocalDate getLocalDate(){
+        return date;
     }
 }
