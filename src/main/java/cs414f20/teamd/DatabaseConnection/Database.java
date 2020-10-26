1a7d87556ea0b3f42a4c1a5053af7bb8a1171469
randomguy6 <32748027+randomguy6@users.noreply.github.com>
2020-10-26 16:35:46 -0600

    Fix port number, move some stuff around, and test

--- src/main/java/cs414f20/teamd/DatabaseConnection/Database.java
+++ src/main/java/cs414f20/teamd/DatabaseConnection/Database.java
@@ -7,14 +7,15 @@ import java.sql.ResultSet;
 
 public class Database {
     // connection information when using port forwarding from local host
-    private final static String DB_URL = "jdbc:mysql://faure.cs.colostate.edu/publicclassteamd";
+    private final static String DB_URL = "jdbc:mysql://127.0.0.1:56247/publicclassteamd";
     private final static String DB_USER = "sdonepud";
-    private final static String DB_PASSWORD = "*";
+    private final static String DB_PASSWORD = "831865987";
     // SQL SELECT query statement
-    private final static String COLUMN = "username";
+    // private final static String COLUMN = "username";
     private final static String QUERY = "SELECT * FROM greatestAccounts;";
-    public static void main(String[] args) {
-         try (
+
+    private static void getAllUsers(){
+        try (
              // connect to the database and query
              Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
              Statement query = conn.createStatement();
@@ -23,10 +24,16 @@ public class Database {
             // iterate through query results and print out the column values
             int count = 0;
             while (results.next()) {
-            System.out.printf("%6d %s\n", ++count, results.getString(COLUMN));
+                System.out.printf("%6d %s", ++count, results.getString("personalID"));
+                System.out.printf("\t%s", results.getString("username"));
+                System.out.printf("\t%s\n", results.getString("password"));
             }
-        } catch (Exception e) {
-         System.err.println("Exception: " + e.getMessage());
+        } 
+        catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
         }
+    }
+    public static void main(String[] args) {
+        getAllUsers();
     }
 }
