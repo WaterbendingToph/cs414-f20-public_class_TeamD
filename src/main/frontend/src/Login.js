8fa8d90e20966fdbb946c6f3276592562aefb9ae
Nick Cunningham <nic1571@rams.colostate.edu>
2020-10-26 10:51:27 -0600

    Add dummy state to Login.js for future integration

--- src/main/frontend/src/Login.js
+++ src/main/frontend/src/Login.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
 
 export default class Login extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            userID: "Nick",
+            password: "password",
+        }
+    }
+
     render() {
         return(
             <div>
