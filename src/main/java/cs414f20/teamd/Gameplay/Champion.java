d29d61893ddda796b5bf755a770a9e25a815eca0
victorem1 <36056568+victorem1@users.noreply.github.com>
2020-11-15 17:01:47 -0700

    Add tests

--- src/main/java/cs414f20/teamd/Gameplay/Champion.java
+++ src/main/java/cs414f20/teamd/Gameplay/Champion.java
@@ -22,20 +22,12 @@ public class Champion extends ChessPiece {
             return "\u2617";
     }
 
+    @Override
     public ArrayList<String> legalMoves() {
-        /**
-         * Returns all legal moves that this piece can make based on the rules of 
-         * this variant of chess (as described in the comment at the top of this
-         * class). Each string in the list should represent a legal destination
-         * for the piece. The order of the moves in the list is arbitrary. If there
-         * are no legal moves, returns an empty ArrayList.
-         * 
-         * @return ArrayList representing the legal moves of the piece from the
-         *         current position
-         * @return An empty ArrayList if there are no legal moves available
-         */
-        
-        System.out.println("Not yet implemented");
-        return new ArrayList<>();
+        ArrayList<String> legalMoves = new ArrayList<String>();
+
+        //
+
+        return legalMoves;
     }
 }
