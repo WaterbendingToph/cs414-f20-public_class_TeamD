abad6caabf14c0937a85c046ac87c4b78ef6bdac
Nick Cunningham <nic1571@rams.colostate.edu>
2020-11-16 13:55:27 -0700

    Starter JS code

--- src/main/frontend/src/Lobby/Ongoing Matches/OngoingMatches.js
+++ src/main/frontend/src/Lobby/Ongoing Matches/OngoingMatches.js
@@ -5,12 +5,42 @@ export default class OngoingMatches extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            userID: "Nick",
-            matches: "Test Matches",
+            userID: this.props.location.state.userID,
+            password: this.props.location.state.password,
+            matches: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch("/matches?userID=" + this.state.userID)
+            .then(res => res.json())
+            .then(result => {
+                this.setState({ matches: result.matches })
+            });
+    }
+
+    populateMatches() {
+        if (this.state.matches.length !== 0) {
+            let allMatches = this.state.matches.map(match => {
+                return (
+                    <div>
+                        <td key={match}>{match} {match[0]} </td>
+                        <td>Go to match</td>
+                        <td key={match}>{match} {match[1]} </td>
+                        <td key={match}>{match} {match[2]} </td>
+                    </div>
+                )
+            });
+            return (
+                <tr>
+                    { allMatches }
+                </tr>
+            )
         }
     }
 
     render() {
+        // If-else to show loading if matches hasn't been loaded yet
         return (
             <div>
                 <h2 style={{ textAlign: "center" }}>Ongoing Matches</h2>
@@ -24,25 +54,7 @@ export default class OngoingMatches extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {/* TODO: Make this a loop that shows each ongoing match for the current user. */}
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Go to Match</td>
-                            <td>Dakota</td>
-                            <td>Your Move</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Go to Match</td>
-                            <td>Victor</td>
-                            <td>Their Move</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Go to Match</td>
-                            <td>Sai</td>
-                            <td>Your Move</td>
-                        </tr>
+                        { this.populateMatches() }
                     </tbody>
                 </Table>
             </div>
