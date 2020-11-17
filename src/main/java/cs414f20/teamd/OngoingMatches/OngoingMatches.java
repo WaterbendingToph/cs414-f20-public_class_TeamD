e11cd5662798141e124b974e83a08561367d5a66
Nick Cunningham <nic1571@rams.colostate.edu>
2020-11-17 15:23:24 -0700

    Add tests for Lobby and OngoingMatches

--- src/main/java/cs414f20/teamd/OngoingMatches/OngoingMatches.java
+++ src/main/java/cs414f20/teamd/OngoingMatches/OngoingMatches.java
@@ -37,7 +37,7 @@ public class OngoingMatches {
             "}";
     }
 
-    void queryDatabase() {
+    public void queryDatabase() {
         this.matches = Database.getOngoingMatches(userID);
     } 
 
