e9b706cbfba3ff0b1518c2ac189703f9668d6631
victorem1 <36056568+victorem1@users.noreply.github.com>
2020-11-30 22:40:30 -0700

    Update KingTest.java

--- src/test/java/cs414f20/teamd/Gameplay/KingTest.java
+++ src/test/java/cs414f20/teamd/Gameplay/KingTest.java
@@ -1,11 +1,6 @@
 package cs414f20.teamd.Gameplay;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class KingTest {
