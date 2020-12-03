0732c523a9e01c36672db1afac1f9bbff9fda9a2
dakota <dakotago@rams.colostate.edu>
2020-12-03 16:28:51 -0700

    correct that number for queen

--- src/main/java/cs414f20/teamd/Gameplay/Queen.java
+++ src/main/java/cs414f20/teamd/Gameplay/Queen.java
@@ -67,7 +67,7 @@ public class Queen extends ChessPiece {
         for (int cardinalDirection = 0; cardinalDirection < 4; cardinalDirection++) {
             potentialPaths.add(new ArrayList<>());
 
-            for (int magnitude = 1; magnitude < 8; magnitude++) {
+            for (int magnitude = 1; magnitude < 10; magnitude++) {
                 try {
                     int xChange = (cardinalDirection % 2) * (1 - 2 * (cardinalDirection / 2)) * magnitude;
                     int yChange = ((cardinalDirection + 1) % 2) * (-1 + 2 * (cardinalDirection / 2)) * magnitude;
