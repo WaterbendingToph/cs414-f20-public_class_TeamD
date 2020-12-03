81b12b4ea5de6c6523d029e6acbade034cf350f9
victorem1 <36056568+victorem1@users.noreply.github.com>
2020-12-03 13:53:57 -0700

    Add inactive castling test

--- src/test/java/cs414f20/teamd/Gameplay/KingTest.java
+++ src/test/java/cs414f20/teamd/Gameplay/KingTest.java
@@ -22,6 +22,7 @@ class KingTest {
         testFreeMovement();
         testCornerMovement();
         testSharedSpaceMovement();
+        //testCastling();
     }
 
     private void testFreeMovement() {
@@ -55,5 +56,62 @@ class KingTest {
         String[] expectedMoves = {"a1", "a2", "a3", "b3", "c1", "c2", "c3"};
 
         TestHelper.assertExpectedMovesEqualLegalMoves(expectedMoves, king.legalMoves());
+    }
+
+    private void testCastling() {
+        testCastlingLeft();
+        testCastlingRight();
+    }
+    private void testCastlingLeft() {
+        requisiteBoard = new ChessBoard();
+
+        King king = new King(requisiteBoard, ChessPiece.Color.WHITE);
+        Rook rook = new Rook(requisiteBoard, ChessPiece.Color.WHITE);
+
+        requisiteBoard.placePiece(king, "f0");
+        requisiteBoard.placePiece(rook, "b0");
+
+        String[] expectedMoves = {"d0", "e0", "e1", "f1", "g0", "g1"};
+
+        TestHelper.assertExpectedMovesEqualLegalMoves(expectedMoves, king.legalMoves());
+
+
+        try {
+            requisiteBoard.move("f0", "d0");
+        } catch (IllegalMoveException e) { fail(); }
+
+        try {
+            ChessPiece testPiece = requisiteBoard.getPiece("d0");
+            assertEquals(king, testPiece);
+
+            testPiece = requisiteBoard.getPiece("e0");
+            assertEquals(rook, testPiece);
+        } catch (IllegalPositionException e) { fail(); }
+    }
+    private void testCastlingRight() {
+        requisiteBoard = new ChessBoard();
+
+        King king = new King(requisiteBoard, ChessPiece.Color.BLACK);
+        Rook rook = new Rook(requisiteBoard, ChessPiece.Color.BLACK);
+
+        requisiteBoard.placePiece(king, "f9");
+        requisiteBoard.placePiece(rook, "i9");
+
+        String[] expectedMoves = {"e8", "e9", "f8", "g8", "g9", "h9"};
+
+        TestHelper.assertExpectedMovesEqualLegalMoves(expectedMoves, king.legalMoves());
+
+
+        try {
+            requisiteBoard.move("f9", "h9");
+        } catch (IllegalMoveException e) { fail(); }
+
+        try {
+            ChessPiece testPiece = requisiteBoard.getPiece("h9");
+            assertEquals(king, testPiece);
+
+            testPiece = requisiteBoard.getPiece("g9");
+            assertEquals(rook, testPiece);
+        } catch (IllegalPositionException e) { fail(); }
     }
 }
