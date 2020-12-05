f13cca1098bb6aad6ea04aa5a4b004cb304a7a9c
victorem1 <36056568+victorem1@users.noreply.github.com>
2020-12-05 15:47:54 -0700

    Delete unused Registration page

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
 import Login from "./Login/Login";
-import Registration from "./Registration";
 import Lobby from "./Lobby/Lobby";
 import OngoingMatches from './Lobby/Ongoing Matches/OngoingMatches';
 import BoardGame from './BoardGame/BoardGame';
@@ -14,7 +13,6 @@ function App() {
         <div id='switches'>
           <Switch>
             <Route exact path="/" component={Login} />
-            <Route exact path="/register" component={Registration} />
             <Route exact path="/lobby" component={Lobby} />
             <Route exact path="/matches" component={OngoingMatches} />
             <Route exact path="/game" component={BoardGame} />
